d06fc929059f883dd3617bcc0f2fa99f4ffd4a2c
PrasantiKadiyala <prasanti.kadiyala@tcs.com>
2020-04-17 02:19:50 +0530

    Commiting the changes for retrieving pass from env

--- Program.cs
+++ Program.cs
@@ -7,54 +7,46 @@ namespace sqliteApp
     {
         static void Main(string[] args)
         {
+            var dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD");
             Console.WriteLine("This app lists the departments and the students records of the college.");
             Console.WriteLine();
-        
-          //  var connectionStringBuilder = new SqliteConnectionStringBuilder();
+
+            //  var connectionStringBuilder = new SqliteConnectionStringBuilder();
             //connectionStringBuilder.DataSource = "sqlitevol/test.db";
 
-            string connstring = string.Format("Server=mysql,3306; database=mysql; UID=root; password=Passw0rd1");
-            
+            string connstring = string.Format("Server=mysql,3306; database=mysql; UID=root; password={0}", dbPass);
 
-            using(var connection = new MySqlConnection(connstring))
+
+            using (var connection = new MySqlConnection(connstring))
             {
                 connection.Open();
 
-                Console.WriteLine("Yay Connection Succeeded!!!");
-
                 var delTableCmd = connection.CreateCommand();
 
-                 delTableCmd.CommandText = "DROP TABLE IF EXISTS STUDENTS;";
-                 delTableCmd.ExecuteNonQuery();
-
-                 Console.WriteLine("Deleted existing Students table ");
+                delTableCmd.CommandText = "DROP TABLE IF EXISTS STUDENTS;";
+                delTableCmd.ExecuteNonQuery();
 
                 delTableCmd.CommandText = "DROP TABLE IF EXISTS DEPARTMENT;";
                 delTableCmd.ExecuteNonQuery();
-                
-                Console.WriteLine("Deleted existing DEPARTMENT table ");
+
 
                 var createTableCmd = connection.CreateCommand();
                 createTableCmd.CommandText = "CREATE TABLE DEPARTMENT(ID int PRIMARY KEY NOT NULL, Name VARCHAR(50));";
                 int createTableResult = createTableCmd.ExecuteNonQuery();
-                
 
-                Console.WriteLine("DEpartment table created");
+               
+                var insertCmd = connection.CreateCommand();
 
-                
-                    Console.WriteLine("Inserting entries in Department Table");
-                    var insertCmd = connection.CreateCommand();
+                insertCmd.CommandText = "INSERT INTO DEPARTMENT VALUES(1,'COMPUTERS');";
+                insertCmd.ExecuteNonQuery();
 
-                    insertCmd.CommandText = "INSERT INTO DEPARTMENT VALUES(1,'COMPUTERS');";
-                    insertCmd.ExecuteNonQuery();
+                insertCmd.CommandText = "INSERT INTO DEPARTMENT VALUES(2,'ELECTRONICS');";
+                insertCmd.ExecuteNonQuery();
 
-                    insertCmd.CommandText = "INSERT INTO DEPARTMENT VALUES(2,'ELECTRONICS');";
-                    insertCmd.ExecuteNonQuery();
+                insertCmd.CommandText = "INSERT INTO DEPARTMENT VALUES(3,'MECHANICAL');";
+                insertCmd.ExecuteNonQuery();
 
-                    insertCmd.CommandText = "INSERT INTO DEPARTMENT VALUES(3,'MECHANICAL');";
-                    insertCmd.ExecuteNonQuery();                   
-                           
-                
+
                 //Read the newly inserted data:
                 var select = connection.CreateCommand();
                 select.CommandText = "SELECT name FROM DEPARTMENT;";
@@ -64,39 +56,36 @@ namespace sqliteApp
                 {
                     while (reader.Read())
                     {
-                        var message = reader.GetString(0);  
+                        var message = reader.GetString(0);
                         Console.WriteLine(message);
                     }
                 }
                 Console.WriteLine();
 
-                createTableCmd.CommandText = "CREATE TABLE STUDENTS ("+
-                                                "ROLLNO int NOT NULL,"+
-                                                "NAME VARCHAR(100) NOT NULL,"+  
+                createTableCmd.CommandText = "CREATE TABLE STUDENTS (" +
+                                                "ROLLNO int NOT NULL," +
+                                                "NAME VARCHAR(100) NOT NULL," +
                                                 "DEPARTMENT int, " +
                                                 "PRIMARY KEY (ROLLNO)," +
-                                                "CONSTRAINT FK_STUDENTDEPT FOREIGN KEY (DEPARTMENT)"+
+                                                "CONSTRAINT FK_STUDENTDEPT FOREIGN KEY (DEPARTMENT)" +
                                                 "REFERENCES DEPARTMENT(ID));";
-    
+
                 int studentTableResult = createTableCmd.ExecuteNonQuery();
-                
-               
-                  
 
-                    insertCmd.CommandText = "INSERT INTO STUDENTS VALUES(1,'SRAVANI',1);";
-                    insertCmd.ExecuteNonQuery();
 
-                    insertCmd.CommandText = "INSERT INTO STUDENTS VALUES(2,'SRAVANTI',2);";
-                    insertCmd.ExecuteNonQuery();
+                insertCmd.CommandText = "INSERT INTO STUDENTS VALUES(1,'SRAVANI',1);";
+                insertCmd.ExecuteNonQuery();
 
-                    insertCmd.CommandText = "INSERT INTO STUDENTS VALUES(3,'JYOTHI',3);";
-                    insertCmd.ExecuteNonQuery();
+                insertCmd.CommandText = "INSERT INTO STUDENTS VALUES(2,'SRAVANTI',2);";
+                insertCmd.ExecuteNonQuery();
 
-                    
+                insertCmd.CommandText = "INSERT INTO STUDENTS VALUES(3,'JYOTHI',3);";
+                insertCmd.ExecuteNonQuery();
+
 
                 //Read the newly inserted data:
                 var selectCmd = connection.CreateCommand();
-                selectCmd.CommandText = "SELECT std.NAME, dept.NAME FROM STUDENTS std "+
+                selectCmd.CommandText = "SELECT std.NAME, dept.NAME FROM STUDENTS std " +
                                        "inner join DEPARTMENT dept on std.DEPARTMENT = dept.ID;";
                 Console.WriteLine("Enrolled students:");
                 using (var reader = selectCmd.ExecuteReader())
@@ -105,11 +94,11 @@ namespace sqliteApp
                     {
                         var student = reader.GetString(0);
                         var department = reader.GetString(1);
-                        Console.WriteLine("Student Name : " + student + "   Department : "+ department);
+                        Console.WriteLine("Student Name : " + student + "   Department : " + department);
                     }
-                } 
-                Console.ReadLine();      
-            }            
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
